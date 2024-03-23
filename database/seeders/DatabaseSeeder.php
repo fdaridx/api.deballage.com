cf4888362853e74868ac6e31ad6b04d0630cd464
Feussi Darid <dfeussidarid@gmail.com>
2024-03-23 15:39:46 +0100

    Troisieme version (Beta) 1.01

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,8 +21,7 @@ class DatabaseSeeder extends Seeder
             CitySeeder::class,
             QwaterSeeder::class,
             AtributeSeeder::class,
-            PropertySeeder::class,
-            AtributeProductSeeder::class
+            PropertySeeder::class
         ]);
     }
 }
